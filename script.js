3d59285e0df444622dacf9ed6f2e34e7e18f8db8
denosaur98 <109144817+denosaur98@users.noreply.github.com>
2023-02-10 12:56:21 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let sliderContainer = document.querySelector("#slider")
+let sliderContainer = document.getElementById("slider1")
 let btns = sliderContainer.querySelectorAll(".line")
 for(let i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     let current = document.querySelectorAll(".line_active")
-    current[1].className = current[0].className.replace("line_active", "")
+    current[0].className = current[0].className.replace("line_active", "")
     this.className += " line_active"
   })
 }

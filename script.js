ae47924483ce4e2c94e4151f07ea80251c982400
denosaur98 <109144817+denosaur98@users.noreply.github.com>
2023-02-10 20:05:31 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ for(let i = 0; i < btns.length; i++) {
     this.className += " line_active"
   })
 }
+
+let btnL = document.querySelector('.arrow_left')
+let btnR = document.querySelector('.arrow_right')
+let slides = document.querySelectorAll('.blocks')
+let sliderCount = slides.length
+let mainContainer = document.querySelector('.main_container')
+let active = 0
+btnL.addEventListener('click', () => {
+  changeSlide('left')
+})
+
+btnR.addEventListener('click', () => {
+  changeSlide('right')
+})
+
+function changeSlide(e) {
+  if(e === 'right') {
+    active--
+    if(active < 0) {
+      active = sliderCount - 1
+    }
+  } else if(e === 'left') {
+    active++
+    if(active === sliderCount) {
+      active = 0
+    }
+  }
+  let width = slides[0].clientWidth
+  mainContainer.style.transform = `translateX(-${active * width}px)`
+}
